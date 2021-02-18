d7712946c657344a5ac47a6a2fcf44566e2407b1
marcelo1811 <miyachimarcelo@gmail.com>
2021-02-18 15:48:11 -0300

    feat: add refetching queries after a successful mutation

--- components/CreateProduct.js
+++ components/CreateProduct.js
@@ -3,6 +3,7 @@ import { gql } from 'graphql-tag'
 import useForm from '../lib/useForm'
 import Form from './styles/form'
 import DisplayError from './ErrorMessage'
+import { ALL_PRODUCTS_QUERY } from './Products'
 
 const CREATE_PRODUCT_MUTATION = gql`
     mutation CREATE_PRODUCT_MUTATION(
@@ -45,6 +46,7 @@ export default function CreateProduct() {
         CREATE_PRODUCT_MUTATION,
         {
             variables: inputs,
+            refetchQueries: [{ query: ALL_PRODUCTS_QUERY }]
         }
     )
     return (
